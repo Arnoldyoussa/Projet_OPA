ddfa9bcd754e93aa10a7b787d38536703a2a248f
Arnoldyoussa <arnoldyoussa@yahoo.fr>
2022-11-27 18:03:05 +0100

    Suppression key API

--- Exemples/Test_file.py
+++ Exemples/Test_file.py
@@ -4,8 +4,6 @@ from binance.lib.utils import config_logging
 
 config_logging(logging, logging.DEBUG)
 
-key = "9NoMNNgwTUU92NbtSwJSuy14z8dXVPyZQQFyEXgjgx6LvqFISaiNdGBQPPXEliSS"
-secret = "rXAtGII8v290i75DsrTZKBYl0RQs7RTlwUBI8gg9YdSXNlf04ZKYAiZZOriybRqU"
 
-spot_client = Client(key, secret)
+spot_client = Client()
 print(spot_client.account_snapshot(type ='SPOT'))
