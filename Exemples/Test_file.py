d7d408c3f0903b4550218cb890eb533a1b38c21c
Arnoldyoussa <arnoldyoussa@yahoo.fr>
2022-11-27 18:06:24 +0100

    suppression fichier

--- Exemples/Test_file.py
+++
@@ -1,9 +0,0 @@
-import logging
-from binance.spot import Spot as Client
-from binance.lib.utils import config_logging
-
-config_logging(logging, logging.DEBUG)
-
-
-spot_client = Client()
-print(spot_client.account_snapshot(type ='SPOT'))
